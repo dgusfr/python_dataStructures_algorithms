999866d611c165f43d9fab21c0ab49c7ef9683ca
dgusfr <diegogustavo.fr@gmail.com>
2026-02-13 12:43:13 -0300

    refactor: simplify swapping logic in bubble_sort function and add pseudocode in teste.txt

--- Sorting_algorithms/bubble_sort.py
+++ Sorting_algorithms/bubble_sort.py
@@ -7,10 +7,7 @@ def bubble_sort(lista):
     for i in range(0, n - 1):
         for j in range(0, n - i - 1):
             if lista[j] > lista[j + 1]:
-                aux = lista[j]
-                lista[j] = lista[j + 1]
-                lista[j + 1] = aux
-
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
     return lista
 
 
