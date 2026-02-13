0f1a71983cd85be944a6b781ca1311a6a95c1d28
dgusfr <diegogustavo.fr@gmail.com>
2026-02-13 12:18:19 -0300

    feat: implement bubble sort algorithm in bubble_sort.py

--- Sorting_algorithms/bubble_sort.py
+++ Sorting_algorithms/bubble_sort.py
@@ -3,3 +3,15 @@ lista = [7, 12, 9, 11, 3]
 
 def bubble_sort(lista):
     n = len(lista)
+
+    for i in range(0, n - 1):
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                aux = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = aux
+
+    return lista
+
+
+print(bubble_sort(lista))
