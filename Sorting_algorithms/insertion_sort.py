10d1e8a64857746581118e2f12be161da2a08b8d
dgusfr <diegogustavo.fr@gmail.com>
2026-02-15 21:43:09 -0300

    feat: improve insertion sort algorithm implementation in insertion_sort.py

--- Sorting_algorithms/insertion_sort.py
+++ Sorting_algorithms/insertion_sort.py
@@ -3,12 +3,16 @@ my_array = [64, 34, 25, 12, 22, 11, 90, 5]
 
 def insertion_sort(arr):
     n = len(my_array)
+
     for i in range(1, n):
-        insert_index = i
-        current_value = my_array.pop(i)
+        insert_index = arr[i]
+        j = i - 1
+
         for j in range(i - 1, -1, -1):
-            if my_array[j] > current_value:
-                insert_index = j
-        my_array.insert(insert_index, current_value)
+            if my_array[j] > insert_index:
+                my_array[j + 1] = my_array[j]
+            else:
+                break
+        my_array[j + 1] = insert_index
 
     print("Sorted array:", my_array)
