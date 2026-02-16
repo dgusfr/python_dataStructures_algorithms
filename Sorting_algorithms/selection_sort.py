54ba10cff47a414f8c81e8bd3d32f5774189dcb0
dgusfr <diegogustavo.fr@gmail.com>
2026-02-15 21:59:23 -0300

    fix: update selection_sort function to use consistent parameter name 'arr' instead of 'array'

--- Sorting_algorithms/selection_sort.py
+++ Sorting_algorithms/selection_sort.py
@@ -1,17 +1,17 @@
 array = [7, 12, 9, 11, 3]
 
 
-def selection_sort(array):
-    n = len(array)
+def selection_sort(arr):
+    n = len(arr)
 
     for i in range(n - 1):
         min_index = i
 
         for j in range(i + 1, n):
-            if array[j] < array[min_index]:
+            if arr[j] < arr[min_index]:
                 min_index = j
-        array[i], array[min_index] = array[min_index], array[i]
-    return array
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
 
 print(selection_sort(array))
