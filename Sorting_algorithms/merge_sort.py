377018160da13b0d6a08996da9b73eacb152ba78
dgusfr <diegogustavo.fr@gmail.com>
2026-02-17 11:56:04 -0300

    feat: implement merge sort algorithm with merge function

--- Sorting_algorithms/merge_sort.py
+++ Sorting_algorithms/merge_sort.py
@@ -1,1 +1,30 @@
 array = [12, 8, 9, 3, 11, 5, 4]
+
+
+def merge_sort(arr, start, end):
+    if start - end > 1:
+        mid = (start + end) // 2
+        merge_sort(arr, start, mid)
+        merge_sort(arr, mid, end)
+
+        return merge(arr, start, mid, end)
+
+
+def merge(arr, start, mid, end):
+    left = arr[start:mid]
+    right = arr[mid:end]
+
+    top_left, top_right = 0, 0
+
+    for k in range(start, end):
+        if top_left < len(left) and (
+            top_right >= len(right) or left[top_left] <= right[top_right]
+        ):
+            arr[k] = left[top_left]
+            top_left += 1
+        else:
+            arr[k] = right[top_right]
+            top_right += 1
+
+
+print(merge_sort(array, 0, len(array)))
